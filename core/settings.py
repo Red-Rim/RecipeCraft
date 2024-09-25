aa7bcdee4b965951a539e3642279c3864b7ec974
Red-Rim <salmaouakhir20@gmail.com>
2024-09-25 23:04:17 +0200

    .

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #'recipecraft.apps.RecipeCraftConfig',
     'recipecraft',  # Add your RecipeCraft app here
-    'myrecipes',
+    #'myrecipes',
 ]
 
 MIDDLEWARE = [
--- core/wsgi.py
+++ core/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for the 'core' project
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+# Set the default settings module for the 'recipecraft' project
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipecraft.settings')
 
 # Create the WSGI application object
 application = get_wsgi_application()
