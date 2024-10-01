4468b14e1f58ee36703c8073f9b4e1c7b76b7fa0
Red-Rim <salmaouakhir20@gmail.com>
2024-10-01 23:51:53 +0100

    images

--- recipecraft/settings.py
+++ recipecraft/settings.py
@@ -125,13 +125,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'public/static'),)
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'public/static'),
+    #os.path.join(BASE_DIR, 'public/static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
--- recipecraft/urls.py
+++ recipecraft/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+     #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
